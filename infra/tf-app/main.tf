9e54e89256f1c119773e27e998365d8f10753f30
joesanthoshalgonquin <sant0232@algonquinlive.com>
2024-04-01 22:56:02 -0400

    Changed name of vnet

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = "canadacentral"
 }
 resource "azurerm_virtual_network" "lab12_vnet" {
-  name                = "lab12-vnet"
+  name                = "lab12vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
